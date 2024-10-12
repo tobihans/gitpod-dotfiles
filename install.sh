ee33383a8d0a4feb1f8a82837151667a335adb79
Hans B. K. Tognon <git@tobihans.space>
2024-10-12 20:08:05 +0100

    feat:dd tree-sitter

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y luarocks
 
 # Neovim
 curl -fL --output /tmp/bob.zip https://github.com/MordechaiHadad/bob/releases/latest/download/bob-linux-x86_64.zip
@@ -15,5 +16,9 @@ cat >~/.config/bob/config.json <<EOF
   "installation_location": "$HOME/.local/bin"
 }
 EOF
+
+curl -fL https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz | gunzip > tree-sitter
+chmod +x tree-sitter
+sudo install -v ./tree-sitter /usr/local/bin/
 
 bob use stable
