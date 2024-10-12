fe4b167317a7fffc7368148650c1ec60f7ae13fc
Hans B. K. Tognon <git@tobihans.space>
2024-10-12 20:30:09 +0100

    feat: simplify neovim installation

--- install.sh
+++ install.sh
@@ -5,20 +5,15 @@ sudo apt upgrade -y
 sudo apt install -y luarocks
 
 # Neovim
-curl -fL --output /tmp/bob.zip https://github.com/MordechaiHadad/bob/releases/latest/download/bob-linux-x86_64.zip
-unzip -d /tmp /tmp/bob.zip
-sudo install -v /tmp/bob-linux-x86_64/bob /usr/local/bin/
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-mkdir -p ~/.local/share/bash-completion/completions ~/.config/bob
-bob complete bash >> ~/.local/share/bash-completion/completions/bob
-cat >~/.config/bob/config.json <<EOF
-{
-  "installation_location": "$HOME/.local/bin"
-}
-EOF
+# shellcheck disable=2016
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 
+# Tree-sitter
 curl -fL https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz | gunzip > tree-sitter
 chmod +x tree-sitter
 sudo install -v ./tree-sitter /usr/local/bin/
 
-bob use stable
